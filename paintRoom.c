9f6fbd899da860d29bb9c818d76725a6a3bba93d
RedRyA <reddoch.ryan@yahoo.com>
2023-02-24 05:08:52 -0600

    need distancefromA

--- paintRoom.c
+++ paintRoom.c
@@ -1,19 +1,19 @@
 #include "paintRoom.h"
 #include <stdbool.h>
 
+/*Brute Force No distance formula*/
 /* if moveup=' ' place letter set to true
 else set to true and turn if turn = ' ' etc.
 */
 
-
-int findDistance(char** roomArr, int ai,int aj,int row, int col);
-void recPaintRoom(char **roomArr, int row, int col, int ai, int aj, int distanceFromA /* feel free to remove/add any other parameters here*/);
+int findDistance(char **roomArr, int row, int col, int ai, int aj, int distanceFromA);
+void recPaintRoom(char **roomArr, int row, int col, int distanceFromA /* feel free to remove/add any other parameters here*/);
 /* declare any other helper functions here*/
-void moveUp(char **roomArr, int row, int col, int distanceFromA, bool);
-void moveDown(char **roomArr, int row, int col, int distanceFromA, bool);
-void turnRight(char **roomArr, int row, int col, int distanceFromA, bool);
-void turnLeft(char **roomArr, int row, int col, int distanceFromA, bool);
-void placeLet(char **roomArr, int distanceFromA, int row, int col,bool visited);
+void moveUp(char **roomArr, int row, int col, int distanceFromA, bool visited, bool noGo);
+void moveDown(char **roomArr, int row, int col, int distanceFromA, bool visited, bool noGo);
+void turnRight(char **roomArr, int row, int col, int distanceFromA, bool visited, bool noGo);
+void turnLeft(char **roomArr, int row, int col, int distanceFromA, bool visited, bool noGo);
+void placeLet(char **roomArr, int row, int col, int distanceFromA, bool visited);
 /* printName
  * input: none
  * output: none
@@ -39,41 +39,32 @@ void paintRoom(RoomData room)
 {
   /* Call any other helper functions (a helper function to find the location of
    * 'A' in room may be handy) */
- 
-  char ** roomArr=room.roomArray; // change to a more manageable name
-  int nrows = room.numrows; 
+
+  char **roomArr = room.roomArray; // change to a more manageable name
+  int nrows = room.numrows;
   int ncols = room.numcols;
   int i, j;
-  int row;
-  int col;
-  int ai, aj;
-  int distanceFromA;
+  int row; // initial staring row for A
+  int col; // initial starting col for A
+int distanceFromA;
   for (i = 0; i < nrows; i++)
   {
     for (j = 0; j < ncols; j++)
     {
-      row=i;
-      col=j;
-    
       if (roomArr[i][j] == 'A')
       {
 
-        ai = i;
-        aj = j;
-        
+        row = i;
+        col = j;
+      int ai=row;
+      int  aj=col;
+     
+     
+        recPaintRoom(roomArr, row, col, distanceFromA);
       }
-
-
-
-   distanceFromA = abs((row-ai) + (col -aj));
- 
-recPaintRoom(roomArr, row, col, ai, aj, distanceFromA);
-
     }
-
+       
   }
-
-
 }
 
 /* Base cases: All places visited boolean set to true.or unreachable*/
@@ -89,143 +80,165 @@ recPaintRoom(roomArr, row, col, ai, aj, distanceFromA);
  * input: the room to process, the row and column of the current location
  * being explored, the distance traveled from 'A' output: N/A
  */
-void recPaintRoom(char **roomArr, int row, int col, int ai, int ji, int distanceFromA /* feel free to remove/add any other parameters here*/)
+void recPaintRoom(char **roomArr, int row, int col, int distanceFromA /* feel free to remove/add any other parameters here*/)
 {
   bool visited;
- printf("95");
-visited=false;
-while(visited=false)
-{
-  // iterate through roomArr
+  bool noGo;
+  noGo = false;
+  visited = false;
+int aRow=row;
+int aCol=col;
 
-  moveUp(roomArr, row, col, distanceFromA, visited);
-  turnRight(roomArr, row, col, distanceFromA, visited);
-  moveDown(roomArr, row, col, distanceFromA, visited);
-  turnLeft(roomArr, row, col, distanceFromA, visited);
+distanceFromA=abs((row-aRow)+(col-aCol));
+ 
+
+  moveUp(roomArr, row, col, distanceFromA, visited, noGo);
+  moveDown(roomArr, row, col, distanceFromA, visited, noGo);
+  turnRight(roomArr, row, col, distanceFromA, visited, noGo);
+  turnLeft(roomArr, row, col, distanceFromA, visited, noGo);
 }
-}
+
 /* My functions */
 
-  
 /* ///////////////// MOVE UP //////////////////// */
 
-void moveUp(char **roomArr, int row, int col, int distanceFromA, bool visited)
+void moveUp(char **roomArr, int row, int col, int distanceFromA, bool visited, bool noGo)
 {
- printf("114");
+  col += 1;
+
   /* move forward in the array if space else try other options */
+
+  if (roomArr[row][col] == ' ')
   {
- printf("117");
-    if (roomArr[row][col] == ' ')
-    {
-
-      placeLet(roomArr, distanceFromA, row, col,visited);
-      moveUp(roomArr, row,  col,  distanceFromA,  visited);
-    }
-    else if (roomArr[row][col] == '*')
-    {
-
-   turnLeft(roomArr, row,  col,  distanceFromA,  visited);
-   
-     }
-    
-    }
-}
-//////////////////////////////////////////
-/* TURN LEFT BY DECREASING ROW*/
-
-void turnLeft(char **roomArr, int row, int col, int distanceFromA, bool visited)
-{
-
-  // move left by deceeasing row  //
-  row - 1;
- 
-    if (roomArr[row][col] == ' ')
-    {
-
-      placeLet(roomArr, distanceFromA, row, col ,visited);
-      moveUp(roomArr, row,  col,  distanceFromA,  visited);
-      
-    }
-    else if (roomArr[row][col] == '*')
-    {
-turnRight(roomArr, row,  col,  distanceFromA,  visited);
-    
-    }
-   
-}
-
-
-
-
-
-
-
-/* TURN RIGHT INCREASE ROW*/
-void turnRight(char **roomArr, int row, int col, int distanceFromA, bool visited)
-{
-  // move right by increase row //
- printf("141");
-  row + 1;
-
   
-    if (roomArr[row][col] == ' ')
-    {
-
-      placeLet(roomArr, distanceFromA, row, col,visited);
-      moveDown(roomArr, row,  col,  distanceFromA,  visited);
-    }
-    else if (roomArr[row][col] == '*')
-    {
-
-      
-    
-    moveDown(roomArr, row,  col,  distanceFromA,  visited);
-    }
-    else
-{
-      visited = true;
+     placeLet(roomArr, row, col, distanceFromA, visited);
+    turnLeft(roomArr, row, col, distanceFromA, visited, noGo);
+    turnRight(roomArr, row, col, distanceFromA, visited, noGo);
+    moveDown(roomArr, row, col, distanceFromA, visited, noGo);
   }
-  
-}
-////////////////////////////////////////
 
-/*MOVE DOWN BY DECREASING COL*/
-void moveDown(char **roomArr, int row, int col, int distanceFromA, bool visited)
-{
- printf("168");
-  // move down  by decreasing column  //
-  col - 1;
-  
-  
-    if (roomArr[row][col] == ' ')
-    {
-
-      placeLet(roomArr, distanceFromA, row, col,visited);
-      
-    }
-    else if (roomArr[row][col] == '*')
-    {
-
-turnLeft(roomArr, row,  col,  distanceFromA,  visited);
-    }
-
-}
-
-
-
-/* PLACE LETTER*/
-void placeLet(char **roomArr, int distanceFromA, int row, int col,bool visited)
-{
-  /* if space place letter */
-  char let = 'A' + distanceFromA;
-  if (distanceFromA <= 24)
+  if (roomArr[row][col] == '*')
   {
-    roomArr[row][col] = let;
-    visited=true;
+    col -= 1;
+    moveDown(roomArr, row, col, distanceFromA, visited, noGo);
   }
   else
   {
-    roomArr[row][col] = 'Z';
-    visited=true;
+
+    moveUp(roomArr, row, col, distanceFromA, visited, noGo);
   }
 }
+
+////////////////////////////////////////
+
+/*MOVE DOWN BY DECREASING COL*/
+void moveDown(char **roomArr, int row, int col, int distanceFromA, bool visited, bool noGo)
+{
+
+  // move down  by decreasing column  //
+  col -= 1;
+
+  if (roomArr[row][col] == ' ')
+  {
+    placeLet(roomArr, row, col, distanceFromA, visited);
+    turnRight(roomArr, row, col, distanceFromA, visited, noGo);
+    turnLeft(roomArr, row, col, distanceFromA, visited, noGo);
+    moveDown(roomArr, row, col, distanceFromA, visited, noGo);
+  }
+
+  if (roomArr[row][col] == '*')
+  {
+
+    col += 1;
+    return;
+  }
+
+  else
+  {
+
+    moveDown(roomArr, row, col, distanceFromA, visited, noGo);
+  }
+}
+
+//////////////////////////////////////////
+/* TURN LEFT BY DECREASING ROW*/
+
+void turnLeft(char **roomArr, int row, int col, int distanceFromA, bool visited, bool noGo)
+{
+
+  // move left by deceeasing row  //
+  row -= 1;
+
+  if (roomArr[row][col] == ' ')
+  {
+    placeLet(roomArr, row, col, distanceFromA, visited);
+    turnRight(roomArr, row, col, distanceFromA, visited, noGo);
+    moveDown(roomArr, row, col, distanceFromA, visited, noGo);
+    moveUp(roomArr, row, col, distanceFromA, visited, noGo);
+  }
+
+  if (roomArr[row][col] == '*')
+  {
+
+    row += 1;
+    return;
+  }
+  else
+  {
+    turnLeft(roomArr, row, col, distanceFromA, visited, noGo);
+  }
+}
+
+/* TURN RIGHT INCREASE ROW*/
+void turnRight(char **roomArr, int row, int col, int distanceFromA, bool visited, bool noGo)
+{
+  // move right by increase row //
+  row += 1;
+
+  if (roomArr[row][col] == ' ')
+  {
+    placeLet(roomArr, row, col, distanceFromA, visited);
+    turnLeft(roomArr, row, col, distanceFromA, visited, noGo);
+    moveDown(roomArr, row, col, distanceFromA, visited, noGo);
+    moveUp(roomArr, row, col, distanceFromA, visited, noGo);
+  }
+  if (roomArr[row][col] == '*')
+  {
+    row -= 1;
+    return;
+  }
+  else
+  {
+    turnRight(roomArr, row, col, distanceFromA, visited, noGo);
+  }
+}
+
+/* PLACE LETTER*/
+void placeLet(char **roomArr, int row, int col, int distanceFromA, bool visited)
+{
+  /* if space place letter */
+
+char let;
+
+   let = 'B'+distanceFromA;
+printf("%d",distanceFromA);
+
+
+
+
+    if (let <= 'Z')
+    {
+      roomArr[row][col] = let;
+    }
+    else if (let >90)
+    {
+    
+     roomArr[row][col] = 'Z';
+      
+    }
+       
+}
+//findDistance(char **roomArr, int row, int col, int ai, int aj, int distanceFromA){
+//distanceFromA=abs()
+
+//}
